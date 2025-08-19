dfa94b90415340228741995076834ad7a71e9c74
Rodrigo502 <84424702+Rodrigo502@users.noreply.github.com>
2025-08-18 23:02:40 -0500

    feat: facial_recognition_service updated

--- app/services/facial_recognition_service.py
+++ app/services/facial_recognition_service.py
@@ -1,6 +1,6 @@
 # services/facial_recognition_service.py
 from app.utils.emotion_recognition import EmotionRecognition
-
+from app.utils.media_pipe_utils import process_image_with_mediapipe, batch_process_images  # Asegúrate de importar batch_process_images
 
 class FacialRecognitionService:
     @staticmethod
@@ -12,3 +12,12 @@ class FacialRecognitionService:
 
         # Retorna el resultado de la emoción detectada
         return response
+
+    @staticmethod  # Añade @staticmethod si la función es estática
+    def analyze_images_for_avatar():
+        input_folder = 'images/raw_images/'
+        output_folder = 'images/processed_images/'
+
+        # Procesar imágenes en lote
+        batch_process_images(input_folder, output_folder)
+        return "Images processed with facial landmarks saved."
