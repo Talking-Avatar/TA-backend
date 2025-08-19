55cff0289de91e8fa6881b21e722ea09d59ff723
Rodrigo502 <84424702+Rodrigo502@users.noreply.github.com>
2025-08-18 23:41:47 -0500

    feat: emotion_recognition updated

--- app/utils/emotion_recognition.py
+++ app/utils/emotion_recognition.py
@@ -1,20 +1,27 @@
 # utils/emotion_recognition.py
 from fer import FER
 import cv2
+import numpy as np
 
 class EmotionRecognition:
     def __init__(self):
-        # Carga el modelo FER preentrenado
+        # Carga del modelo FER preentrenado con MTCNN activado
         self.detector = FER(mtcnn=True)
 
+    def preprocess_image(self, image):
+        # Convertir a escala de grises
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Ajustar brillo y contraste
+        adjusted_image = cv2.convertScaleAbs(gray_image, alpha=1.5, beta=20)  # Parámetros ajustables
+        # Volver a convertir a BGR para mantener compatibilidad con MTCNN
+        return cv2.cvtColor(adjusted_image, cv2.COLOR_GRAY2BGR)
+
     def detect_emotion(self, image):
-        """
-        Detecta la emoción en la imagen proporcionada.
-        :param image: Imagen (fotograma) en formato numpy array (capturada con OpenCV).
-        :return: Diccionario con la emoción detectada y su confianza.
-        """
-        # Detecta emociones en la imagen
-        results = self.detector.top_emotion(image)
+        # Preprocesar imagen
+        preprocessed_image = self.preprocess_image(image)
+        # Detectar emociones en la imagen preprocesada
+        results = self.detector.top_emotion(preprocessed_image)
+
         if results:
             emotion, score = results
             return {
